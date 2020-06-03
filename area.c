51f7dca62a41fea243aee091161f998e0f7df63d
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-06-03 21:49:33 +0530

    Square area program in c

---
+++ area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+void main(){
+  int pie,radius,area; 
+  printf("enter the radius of the circle\n");
+  scanf("%d",&radius);
+  pie=3.1415;
+  area=pie*radius*radius;
+  printf("the area of the circle is %d",area);
+  
+}

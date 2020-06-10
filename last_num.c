03bc86e3e01d39995a0ce8909ff9bff07b404214
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-06-10 22:13:11 +0530

    Find the last right most number program in c

---
+++ last_num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+  float n;
+  int num;
+  printf("enter num to find right most integer");
+  scanf("%f",&n);
+  num=(int)n%10;
+  printf("%d",num);
+} 

fc7dc764eb339142270a07bf8d5b0351e50f5ef6
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-06-21 23:48:00 +0530

    Pattern program in c

---
+++ pattern_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+  int i,j;
+  printf("the pattern program in c\n");
+  printf("----------------------\n");
+  for(i=0;i<3;i++){
+    for(j=0;j<4;j++){
+      printf("*\t\t\v\v");
+    }
+    printf("\n");
+  }
+  
+}

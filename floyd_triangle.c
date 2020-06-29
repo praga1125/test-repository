ee28fe8770ce4701f95236003a025fb9f66c07de
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-06-29 21:46:54 +0530

    Floyd's triangle in 0 and 1 program in c

--- floyd_triangle.c
+++ floyd_triangle.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <conio.h>
 void main(){
-  int row,i,j,num=1;
+  int row,i,j,p,q;
   printf("enter the number of row\n");
   scanf("%d",&row);
-  printf("the floyd's triangle :\n");
+  printf("the Floyd triangle:\n");
   printf("--------------------\n");
-  for(i=0;i<=row;i++){
-    for(j=0;j<=i;++j){
-      printf("%d\t",num);
-      ++num;
-    }
-    printf("\n");
-  }
+  for(i=1;i<=row;i++){
+     if(i%2==0){ 
+       p=1;
+       q=0;
+     }
+     else{
+        p=0;
+        q=1;
+     }
+      for(j=1;j<=i;j++)
+	 if(j%2==0)
+	    printf("%d",p);
+	 else
+	    printf("%d",q);
+      printf("\n");
+     
+   }
 }

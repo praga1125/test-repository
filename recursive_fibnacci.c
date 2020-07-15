8162b4530b67ae3a1508bd21b26869529247e337
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-07-15 22:17:04 +0530

    Recursive function program in c

---
+++ recursive_fibnacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+  int n, i = 0, c;
+  printf("enter the limit to fibonacci series\n");
+  scanf("%d", &n);
+  printf("fibonacci series terms are:\n");
+  for (c = 1; c <= n; c++)
+  {
+    printf("%d\n", fabi(i));
+    i++;
+  }
+  return 0;
+}
+
+int fabi(int n)
+{
+  if (n == 0 || n == 1)
+    return n;
+  else
+    return (fabi(n-1) + fabi(n-2));
+}

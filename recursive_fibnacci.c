16312496e9b0ec9e1f69fe35463635fced0e086c
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2020-07-15 22:22:27 +0530

    Update recursive_fibnacci.c

--- recursive_fibnacci.c
+++ recursive_fibnacci.c
@@ -2,11 +2,11 @@
 #include <conio.h>
 int main(){
   int n, i = 0, c;
-  printf("enter the limit to fibonacci series\n");
+  printf("enter the limit to fibonacci series \n");
   scanf("%d", &n);
-  printf("fibonacci series terms are:\n");
+  printf("fibonacci series terms are: \n");
   for (c = 1; c <= n; c++)
-  {
+ {
     printf("%d\n", fabi(i));
     i++;
   }
